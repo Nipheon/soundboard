7d54bf5036a401552867c1b3e5402f830d997649
Nipheon <ajoscht@gmx.de>
2014-08-25 20:00:17 +0200

    * added css triggers * sounds playing limited to one * clean ups

--- js/soundboard.js
+++ js/soundboard.js
@@ -1,9 +1,22 @@
+// create shortcut for soundjs
+var audio = createjs.Sound;
+
 var soundboard = (function () {
 
-	// private map for the sounds
+	// private map for the sound instances
 	var soundInstances = {};
-	// private map for key presses
+	// private map for mapped keys
 	var keymap = {};
+	// helper, used to limit sounds to 1
+	var whatsPlaying;
+
+	// function called when a sound finishs playing
+	var playFinished = function () {
+		// remove the playing class from the html element
+		$('#' + whatsPlaying.id).removeClass('playing');
+		// delete the currently played sound instance
+		whatsPlaying = false;
+	};
 
 	// public methods
 	return {
@@ -15,20 +28,26 @@ var soundboard = (function () {
 			audio.alternateExtensions = ["mp3"];
 
 
-			// go through each button
+			// go through each play button
 			$('.play').each(function (i, element) {
 
 				// read name and sourcefile
 				var soundName = $(element).data('sound');
-				// load sound and give id
-				audio.registerSound(audioPath + soundName + '.ogg', soundName);
+				// load sound and give name
+				audio.registerSound(audioPath + soundName + '.ogg', soundName, soundName);
 				// create instance for this sound
 				soundInstances[soundName] = audio.createInstance(soundName);
+				// attach soundName to each instance because soundjs is stupid and does not attach the id
+				// to events
+				soundInstances[soundName].id = soundName;
 
-				// register click handler for button
+				// write id to html element
+				$(element).attr('id', soundName);
+
+				// register click handler for element
 				$(element).click(function () {
 
-					soundboard.playAudio($(this).data('sound'));
+					soundboard.playAudio(soundName);
 
 				});
 
@@ -42,7 +61,7 @@ var soundboard = (function () {
 			// add listener for keyboard presses
 
 			$(document).bind('keydown', function (e) {
-				//check if key is in the map and then call soundinstance
+				//check if key is in the map and then call player function
 				if (keymap[e.keyCode]) {
 					soundboard.playAudio(keymap[e.keyCode])
 				}
@@ -51,17 +70,35 @@ var soundboard = (function () {
 
 		},
 
-		playAudio: function (id) {
+		playAudio: function (soundName) {
 
-			// check if sound is not currently playing
-			if ((soundInstances[id].playState) != 'playSucceeded') {
-				// stop all other sounds
-				audio.stop();
-				//play sound
-				soundInstances[id].play();
-			// else stop all sounds
-			} else {
-				audio.stop();
+			// check if a sound is currently playing
+			if (whatsPlaying) {
+				// stop, hammertime
+				whatsPlaying.stop();
+				whatsPlaying.removeAllEventListeners();
+				// reset style
+				$('#' + whatsPlaying.id).removeClass('playing');
+			}
+
+			// was the trigger for the same sound?
+			if (whatsPlaying == soundInstances[soundName])
+			// sound is already stopped, just destroy helper
+			{
+				whatsPlaying = false;
+			}
+
+			// play the new sound
+			else {
+
+				// copy the requested sound to the helper
+				whatsPlaying = soundInstances[soundName];
+				// start playing
+				whatsPlaying.play();
+				// add event for playack  complete
+				whatsPlaying.addEventListener("complete", playFinished);
+				// toggle css
+				$('#' + soundName).addClass('playing');
 			}
 		}
 
@@ -71,14 +108,11 @@ var soundboard = (function () {
 })();
 
 
-
 $(document).ready(function () {
-
-	// create shortcut for soundjs
-	audio = createjs.Sound;
 
 	soundboard.init();
 
 });
+
 
 
